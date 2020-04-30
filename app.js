d9f39384a8d8a646158e23547605a8bf6a0acee3
bhaskar809 <bhaskar809@gmail.com>
2020-04-30 09:44:49 +0530

    added methods to read inputs and exposed the private object to other controller

--- app.js
+++ app.js
@@ -7,13 +7,36 @@ var budgerController = (function(){
 
  // UI CONTROLLER
 var UIController = (function(){
+    var DOMstrings = {
+        inputType: '.add__trype',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+    return {
+        getInput: function(){
 
+            return{
+            type: document.querySelector(DOMstrings.inputType).value,//either inc or exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+        };
+
+    },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
 
  //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
+
+    var DOM = UICtrl.getDOMstrings;
     var ctrlAddItem = function(){
           // 1. get the filed input data
+          var inputData = UICtrl.getInput();
+          console.log(inputData);
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
@@ -21,7 +44,7 @@ var controller = (function(budgetCtrl, UICtrl){
 
         
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress',function(event){
         if (event.keyCode===13 || event.which===13){
